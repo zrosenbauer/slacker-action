bd33c7631c93f13d3a903468e673cc649d6b72c7
Zac Rosenbauer <zacrosenbauer@gmail.com>
2021-03-07 20:46:56 -0600

    init

---
+++ src/lib/slack.ts
@@ -0,0 +1,36 @@
+import { IncomingWebhook } from '@slack/webhook';
+
+export interface Input {
+  text: string;
+  // eslint-disable-next-line camelcase
+  webhook_url?: string;
+}
+
+export interface Config {
+  webhookUrl: string;
+  text: string;
+  channel?: string;
+  username?: string;
+  iconEmoji?: string;
+  iconUrl?: string;
+}
+
+export function buildConfig (input: Input): Config {
+  const webhookUrl = process.env.WEBHOOK_URL ?? input.webhook_url;
+  if (!webhookUrl) {
+    throw new Error('"webhook_url" input must be set');
+  }
+
+  return {
+    webhookUrl,
+    text: input.text
+  };
+}
+
+export async function sendMessage (webhookUrl: string, config: Config): Promise<void> {
+  const webhook = new IncomingWebhook(config.webhookUrl);
+
+  await webhook.send({
+    text: config.text
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
-console.log('This is my action...');
+import * as core from '@actions/core';
+
+import * as slack from './lib/slack';
+
+const availableInputs = [
+  'username',
+  'channel',
+  'text',
+  'webhook_url'
+];
+
+(async function () {
+  try {
+    const webhookUrl = core.getInput('webhook_url');
+    const config = {} as slack.Config;
+
+    availableInputs.forEach((availableInput) => {
+      config[availableInput as keyof slack.Config] = core.getInput(availableInput);
+    });
+
+    await slack.sendMessage(webhookUrl, config);
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+})();
