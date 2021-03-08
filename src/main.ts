68da87ad38d86caa5ecdfc2815bc0f05a28b478f
Zac Rosenbauer <zac@bluenova.io>
2021-03-07 23:53:26 -0600

    Initial setup (#1)
    Initial Setup....lots of commits all squashed into one yaayyyy 😄

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log('This is my action...');
+import * as core from '@actions/core';
+
+import * as slack from './lib/slack';
+
+const availableInputs = ['username', 'text', 'icon_emoji', 'icon_url'];
+
+(async function () {
+  try {
+    const webhookUrl = process.env.WEBHOOK_URL
+      ? process.env.WEBHOOK_URL
+      : core.getInput('webhook_url');
+
+    if (!webhookUrl) {
+      throw new Error('"webhook_url" input must be set');
+    }
+
+    const config = {} as slack.Input;
+
+    availableInputs.forEach(availableInput => {
+      const value = core.getInput(availableInput);
+
+      if (value) {
+        config[availableInput as keyof slack.Input] = value;
+      }
+    });
+
+    await slack.sendMessage(webhookUrl, config);
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+})();
