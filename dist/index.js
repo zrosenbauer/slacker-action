7aae357ae0f72983b813e186c63b67b268866799
Zac Rosenbauer <zacrosenbauer@gmail.com>
2021-03-07 22:03:39 -0600

    fixed again

--- dist/index.js
+++ dist/index.js
@@ -8,22 +8,15 @@ require('./sourcemap-register.js');module.exports =
 "use strict";
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.sendMessage = exports.buildConfig = void 0;
+exports.sendMessage = void 0;
 const webhook_1 = __nccwpck_require__(1095);
-function buildConfig(input) {
-    return {
-        text: input.text,
-        username: input.username
-    };
-}
-exports.buildConfig = buildConfig;
 async function sendMessage(webhookUrl, config) {
     const webhook = new webhook_1.IncomingWebhook(webhookUrl);
     await webhook.send({
         text: config.text,
         username: config.username,
-        icon_emoji: config.iconEmoji,
-        icon_url: config.iconUrl
+        icon_emoji: config.icon_emoji,
+        icon_url: config.icon_url
     });
 }
 exports.sendMessage = sendMessage;
