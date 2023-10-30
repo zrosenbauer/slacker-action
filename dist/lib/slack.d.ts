a8aa4f60cfbbe0d31eaf053cbc3d5095748bc581
Zac Rosenbauer <zac@joggr.io>
2023-10-30 13:35:13 -0400

    Release | v1.5.0 (#428)

--- dist/lib/slack.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Input {
-    text?: string;
-    username?: string;
-    icon_emoji?: string;
-    icon_url?: string;
-}
-export declare function sendMessage(webhookUrl: string, input: Input): Promise<void>;
